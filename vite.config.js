cf29500e7c99e48075f8ac14471ecca368ed2cd4
sunsh0345 <sunsh45@naver.com>
2025-08-28 22:53:55 +0900

    경로수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/myportfolio/',
+  // base: '/myportfolio/', // github pages 경로용
+  base: './',
 })
